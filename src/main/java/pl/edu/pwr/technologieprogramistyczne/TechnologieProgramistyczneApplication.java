8e4f6623cafcce664f4446b9407da48cfafb2f4f
kdywan_254049 <idywan_254049@e-science.pl>
2022-09-14 19:06:11 +0200

    Startup

---
+++ src/main/java/pl/edu/pwr/technologieprogramistyczne/TechnologieProgramistyczneApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.technologieprogramistyczne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologieProgramistyczneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologieProgramistyczneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pwr/technologieprogramistyczne/TechnologieProgramistyczneApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.technologieprogramistyczne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologieProgramistyczneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
